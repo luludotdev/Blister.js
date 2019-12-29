5498cc624e5fc29731a9806d0f0df718e4911956
Jack Baron <jackmbaron@gmail.com>
2019-12-29 17:18:53 +0000

    use more robust base64 regex

--- src/binary.ts
+++ src/binary.ts
@@ -1,6 +1,6 @@
 import { ERR_INVALID_BASE64 } from './errors'
 
-const b64RX = /^.+base64,(.+)$/
+const b64RX = /^(?:data:\w+\/\w+;base64,)?((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)$/
 
 export const b64ToBuffer: (str: string) => Buffer = str => {
   if (typeof str !== 'string') throw ERR_INVALID_BASE64
